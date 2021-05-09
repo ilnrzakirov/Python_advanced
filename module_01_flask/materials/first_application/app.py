5c74cb85b37fb57f70266428f150cd68030ebd9c
Ilnur <ilnrzakirov@gmail.com>
2021-05-09 18:36:30 +0300

    task1

--- module_01_flask/materials/first_application/app.py
+++ module_01_flask/materials/first_application/app.py
@@ -3,7 +3,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/test')
+@app.route('/hello')
 def test_function():
    return 'Это тестовая страничка, ответ сгенерирован в %s' % \
                      datetime.datetime.now().utcnow()
+
+@app.route('/test')
+def hello():
+   return 'Привет мир'
