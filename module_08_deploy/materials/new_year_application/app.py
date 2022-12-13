5c8de1d72d0614a0df612cce38ed6cf461f0a406
Ilnur <ilnrzakirov@gmail.com>
2022-12-13 21:57:38 +0300

    add static

--- module_08_deploy/materials/new_year_application/app.py
+++ module_08_deploy/materials/new_year_application/app.py
@@ -5,9 +5,9 @@ from flask import Flask, render_template, send_from_directory
 root_dir = os.path.dirname(os.path.abspath(__file__))
 
 template_folder = os.path.join(root_dir, "templates")
-js_directory = os.path.join(template_folder, "js")
-css_directory = os.path.join(template_folder, "css")
-images_directory = os.path.join(template_folder, "images")
+js_directory = os.path.join(template_folder, "../static/js")
+css_directory = os.path.join(template_folder, "../static/css")
+images_directory = os.path.join(template_folder, "../static/images")
 
 app = Flask(__name__, template_folder=template_folder)
 
@@ -17,17 +17,17 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/js/<path:path>")
+@app.route("/static/js/<path:path>")
 def send_js(path):
     return send_from_directory(js_directory, path)
 
 
-@app.route("/css/<path:path>")
+@app.route("/static/css/<path:path>")
 def send_css(path):
     return send_from_directory(css_directory, path)
 
 
-@app.route("/images/<path:path>")
+@app.route("/static/images/<path:path>")
 def send_images(path):
     return send_from_directory(images_directory, path)
 
