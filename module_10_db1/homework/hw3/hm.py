bec7566e935ffc55834ebe419a5e2b5318f30be7
Ilnur <ilnrzakirov@gmail.com>
2022-12-23 06:23:01 +0300

    len result

--- module_10_db1/homework/hw3/hm.py
+++ module_10_db1/homework/hw3/hm.py
@@ -4,6 +4,15 @@ with sqlite3.connect("hw_3_database.db") as conn:
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM 'table_1'")
     result = cursor.fetchall()
-    print(result)
+    cursor.execute("SELECT * FROM 'table_2'")
+    result_2 = cursor.fetchall()
+    cursor.execute("SELECT * FROM 'table_3'")
+    result_3 = cursor.fetchall()
+    table_1_size = len(result)
+    table_2_size = len(result_2)
+    table_3_size = len(result_3)
+    print(table_1_size)
+    print(table_2_size)
+    print(table_3_size)
 
 
