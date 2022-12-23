a2a240d198c4fc5a9ccff2889a85f3dc2f87c0f2
Ilnur <ilnrzakirov@gmail.com>
2022-12-23 06:46:16 +0300

    len result

--- module_10_db1/homework/hw3/hm.py
+++ module_10_db1/homework/hw3/hm.py
@@ -13,12 +13,15 @@ with sqlite3.connect("hw_3_database.db") as conn:
     table_3_size = len(result_3)
     cursor.execute("SELECT DISTINCT id FROM 'table_1'")
     unique_record_table_1 = cursor.fetchall()
-    cursor.execute("SELECT COUNT(table_1.id) FROM 'table_1' JOIN table_2 t on table_1.id = t.id WHERE table_1.id == t.id")
+    cursor.execute("SELECT COUNT(table_1.id) FROM 'table_1' JOIN table_2 t on table_1.id = t.id")
     repetition_table_1_2 = cursor.fetchall()
+    cursor.execute("SELECT COUNT(*) FROM 'table_1' JOIN table_2 t on table_1.id = t.id JOIN table_3 t2 on table_1.id = t2.id")
+    repetition_table_1_2_3 = cursor.fetchall()
     print(f"Длина таблицы 1{table_1_size}")
     print(f"Длина таблицы 2{table_3_size}")
     print(f"Длина таблицы 3{table_3_size}")
     print(f"Уникальных записей в таблице 1{len(unique_record_table_1)}")
-    print(f"Повторений: {repetition_table_1_2}")
+    print(f"Повторений между 1 и 2 таблицей: {repetition_table_1_2}")
+    print(f"Повторений между 1 и 2 и 3 таблицей: {repetition_table_1_2_3}")
 
 
