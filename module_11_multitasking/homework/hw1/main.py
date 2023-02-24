ab323f43a42b3bdd9c631e073ce0671d2c75b9dd
Ilnur <ilnrzakirov@gmail.com>
2023-02-24 14:27:24 +0300

    Решение с помощью контекст менеджера

--- module_11_multitasking/homework/hw1/main.py
+++ module_11_multitasking/homework/hw1/main.py
@@ -8,6 +8,17 @@ logging.basicConfig(level='INFO')
 logger: logging.Logger = logging.getLogger(__name__)
 
 
+class ForkManager:
+    def __init__(self, lock: threading.Lock) -> None:
+        self.lock = lock
+
+    def __enter__(self):
+        self.lock.acquire()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.lock.release()
+
+
 class Philosopher(threading.Thread):
     running: bool = True
 
@@ -21,19 +32,26 @@ class Philosopher(threading.Thread):
             logger.info(f'Philosopher {self.name} start thinking.')
             time.sleep(random.randint(1, 10))
             logger.info(f'Philosopher {self.name} is hungry.')
-            try:
-                self.left_fork.acquire()
+            with ForkManager(self.left_fork) as l_fork:
                 logger.info(f'Philosopher {self.name} acquired left fork')
                 if self.right_fork.locked():
                     continue
-                try:
-                    self.right_fork.acquire()
+                with ForkManager(self.right_fork) as r_fork:
                     logger.info(f'Philosopher {self.name} acquired right fork')
                     self.dining()
-                finally:
-                    self.right_fork.release()
-            finally:
-                self.left_fork.release()
+            # try:
+            #     self.left_fork.acquire()
+            #     logger.info(f'Philosopher {self.name} acquired left fork')
+            #     if self.right_fork.locked():
+            #         continue
+            #     try:
+            #         self.right_fork.acquire()
+            #         logger.info(f'Philosopher {self.name} acquired right fork')
+            #         self.dining()
+            #     finally:
+            #         self.right_fork.release()
+            # finally:
+            #     self.left_fork.release()
 
     def dining(self) -> None:
         logger.info(f'Philosopher {self.name} starts eating.')
