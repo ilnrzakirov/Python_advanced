9226d93ed16c649dc37d2613c0255e2085d61afe
Ilnur <ilnrzakirov@gmail.com>
2023-03-09 18:05:19 +0300

    hm2 add proc counter

--- module_12_multitasking_2/homework/hw2/main.py
+++ module_12_multitasking_2/homework/hw2/main.py
@@ -1,10 +1,22 @@
+import subprocess
+
+
 def process_count(username: str) -> int:
-    # количество процессов, запущенных из-под
-    # текущего пользователя username
-    pass
+    proc_count = 0
+    ps = subprocess.run(["ps", f"-U {username}"], capture_output=True)
+    res = subprocess.run(["wc", "-l"], input=ps.stdout, capture_output=True)
+    try:
+        proc_count = int(res.stdout.decode("utf-8"))
+    except Exception:
+        print("Ошибка")
+    return proc_count
 
 
 def total_memory_usage(root_pid: int) -> float:
     # суммарное потребление памяти древа процессов
     # с корнем root_pid в процентах
     pass
+
+
+if __name__ == "__main__":
+    print(process_count("ilnur"))
