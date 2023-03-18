d6e6c9880e485f89e9edcdb3f8f8aa777924d3c2
Ilnur <ilnrzakirov@gmail.com>
2023-03-18 11:49:24 +0300

    add hm2 delete fees

--- module_13_db2/homework/hw2/main.py
+++ module_13_db2/homework/hw2/main.py
@@ -1,3 +1,4 @@
+import csv
 import sqlite3
 
 
@@ -5,7 +6,19 @@ def delete_wrong_fees(
         cursor: sqlite3.Cursor,
         wrong_fees_file: str,
 ) -> None:
-    ...
+    param: list[dict] = []
+    with open(wrong_fees_file, newline="\n") as file:
+        reader = csv.reader(file, delimiter=",")
+        next(reader)
+        for line in reader:
+            param.append(
+                {"time": line[1], "number": line[0]},
+            )
+    query = """
+        DELETE FROM table_fees
+        WHERE (timestamp= :time AND truck_number= :number)
+    """
+    cursor.executemany(query, param)
 
 
 if __name__ == "__main__":
