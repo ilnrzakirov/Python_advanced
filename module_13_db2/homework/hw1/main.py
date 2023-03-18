f51adf7f158f76232f0816027b3012c94d39145c
Ilnur <ilnrzakirov@gmail.com>
2023-03-18 11:07:14 +0300

    add hm1 vaccine

--- module_13_db2/homework/hw1/main.py
+++ module_13_db2/homework/hw1/main.py
@@ -5,13 +5,18 @@ def check_if_vaccine_has_spoiled(
         cursor: sqlite3.Cursor,
         truck_number: str,
 ) -> bool:
-    ...
+    query = f"""
+    SELECT COUNT(*) > 3 FROM table_truck_with_vaccine tb
+        WHERE tb.truck_number = '{truck_number}' AND tb.temperature_in_celsius NOT BETWEEN 16 AND 20
+    """
+    cursor.execute(query)
+    return cursor.fetchone()[0]
 
 
-if __name__ == '__main__':
-    truck_number: str = input('Введите номер грузовика: ')
-    with sqlite3.connect('../homework.db') as conn:
+if __name__ == "__main__":
+    truck_number: str = input("Введите номер грузовика: ")
+    with sqlite3.connect("../homework.db") as conn:
         cursor: sqlite3.Cursor = conn.cursor()
         spoiled: bool = check_if_vaccine_has_spoiled(cursor, truck_number)
-        print('Испортилась' if spoiled else 'Не испортилась')
+        print("Испортилась" if spoiled else "Не испортилась")
         conn.commit()
