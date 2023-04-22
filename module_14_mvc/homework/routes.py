7f064d72136b4c98dfdfa036fef4a2c15e940402
Ilnur <ilnrzakirov@gmail.com>
2023-04-22 14:12:30 +0300

    add hm2

--- module_14_mvc/homework/routes.py
+++ module_14_mvc/homework/routes.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
 from typing import List
 from flask import request, Response
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import InputRequired
 
 from models import init_db, get_all_books, DATA, Book, add_book
 
@@ -38,20 +41,30 @@ def all_books() -> str:
     )
 
 
+class AddBookForm(FlaskForm):
+    book_title = StringField(validators=[InputRequired()])
+    author_name = StringField(validators=[InputRequired()])
+
+
 @app.route('/books/form', methods=['GET', 'POST'])
 def get_books_form() -> str | Response:
     if request.method == 'GET':
         return render_template('add_book.html')
     elif request.method == "POST":
-        book = Book(
-            title=request.form["book_title"],
-            author=request.form["author_name"],
-            id=None
-        )
-        add_book(book)
-        return Response({"msg": "Ok"}, status=200, mimetype='application/json')
+        form = AddBookForm(request.form)
+        if form.validate_on_submit():
+            book = Book(
+                title=request.form["book_title"],
+                author=request.form["author_name"],
+                id=None
+            )
+            add_book(book)
+            return Response({"msg": "Ok"}, status=200, mimetype='application/json')
+        else:
+            return Response(status=418)
 
 
 if __name__ == '__main__':
     init_db(DATA)
+    app.config["WTF_CSRF_ENABLED"] = False
     app.run(debug=True)
