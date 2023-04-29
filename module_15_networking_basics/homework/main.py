b8d33b82626833cd8faca6b14a2d7aeb29a39276
Ilnur <ilnrzakirov@gmail.com>
2023-04-29 15:43:59 +0300

    add rout

--- module_15_networking_basics/homework/main.py
+++ module_15_networking_basics/homework/main.py
@@ -1,12 +1,54 @@
-from flask import Flask, render_template
+import wtforms_json
+from flask import Flask
+from flask import request, Response
+from flask import jsonify
+from wtforms import Form, IntegerField
+from wtforms.validators import InputRequired
 
-from module_15_networking_basics.homework.models import init_db
+
+from module_15_networking_basics.homework.models import init_db, Room, add_room_to_db, get_rooms
 
 app: Flask = Flask(__name__)
+wtforms_json.init()
 
+
+class RoomForm(Form):
+    floor = IntegerField(validators=[InputRequired()])
+    beds = IntegerField(validators=[InputRequired()])
+    guestNum = IntegerField(validators=[InputRequired()])
+    price = IntegerField(validators=[InputRequired()])
+
+
+@app.route('/add-room', methods=['POST'])
+def add_room() -> Response:
+    if request.method == "POST":
+        data = request.get_json()
+        room = Room(
+            floor=data["floor"],
+            beds=data["beds"],
+            guestNum=data["guestNum"],
+            price=data["price"]
+        )
+        add_room_to_db(room)
+        return Response(status=200)
+
+@app.route('/room', methods=['GET'])
+def get_room() -> Response:
+    rooms = get_rooms()
+    properties: dict = {}
+    properties["rooms"] = []
+    for room in rooms:
+        properties["rooms"].append({
+            "id": room.id,
+            "floor": room.floor,
+            "beds": room.beds,
+            "questNum": room.guestNum,
+            "price": room.price
+        })
+    return jsonify(properties)
 
 
 if __name__ == '__main__':
     init_db()
     app.config["WTF_CSRF_ENABLED"] = False
-    app.run(debug=True)
+    app.run(debug=True, port=5000, host="127.0.0.1")
--- module_15_networking_basics/homework/models.py
+++ module_15_networking_basics/homework/models.py
@@ -1,4 +1,50 @@
+import sqlite3
+from typing import Optional, Any
 
+
+class Room:
+    def __init__(self, floor: int, beds: int, guestNum: int, price: int, pk: Optional[int] = None):
+        self.id: int = pk
+        self.floor: int = floor
+        self.beds: int = beds
+        self.guestNum: int = guestNum
+        self.price: int = price
+
+    def __getitem__(self, item: str) -> Any:
+        return getattr(self, item)
 
 def init_db() -> None:
-    pass
+    with sqlite3.connect('table_room.db') as conn:
+        cursor: sqlite3.Cursor = conn.cursor()
+        cursor.execute(
+            """
+                CREATE TABLE IF NOT EXISTS `table_rooms` (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                    floor INTEGER, 
+                    beds INTEGER,
+                    guestNum INTEGER,
+                    price INTEGER
+                );
+            """
+        )
+
+
+def add_room_to_db(room: Room):
+    with sqlite3.connect('table_room.db') as conn:
+        cursor: sqlite3.Cursor = conn.cursor()
+        query = f"""
+                  INSERT INTO table_rooms (floor, beds, guestNum, price) VALUES 
+                  (?, ?, ?, ?)
+                      """
+        cursor.execute(query, (room.floor, room.beds, room.guestNum, room.price))
+
+
+def get_rooms() -> list[Room]:
+    with sqlite3.connect('table_room.db') as conn:
+        cursor: sqlite3.Cursor = conn.cursor()
+        cursor.execute(
+            """
+            SELECT * from `table_rooms`
+            """
+        )
+        return [Room(*row) for row in cursor.fetchall()]
